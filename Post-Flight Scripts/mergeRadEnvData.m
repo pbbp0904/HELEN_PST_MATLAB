1ee75975e3e9b0a69cdb4f28d321d188dd09f104
xcoza <xcoza@DESKTOP-HM88LRH>
2021-11-03 16:56:48 -0500

    Manual Addition of EnvInterp Column Names

--- Post-Flight Scripts/mergeRadEnvData.m
+++ Post-Flight Scripts/mergeRadEnvData.m
@@ -37,11 +37,40 @@ for i = 2:2:4
         xq = radSeconds;
         EnvDataInterp(:,m) = interp1(x,v,xq);
     end
-
+    
+    PayloadRadData{i}.radSeconds = radSeconds';
     PayloadRadData{i}.PacketNum = EnvDataInterp(:,1);
-    
-    
-    
+    PayloadRadData{i}.Pitch = EnvDataInterp(:,2);
+    PayloadRadData{i}.Roll = EnvDataInterp(:,3);
+    PayloadRadData{i}.Yaw = EnvDataInterp(:,4);
+    PayloadRadData{i}.AccX = EnvDataInterp(:,5);
+    PayloadRadData{i}.AccY = EnvDataInterp(:,6);
+    PayloadRadData{i}.AccZ = EnvDataInterp(:,7);
+    PayloadRadData{i}.GyroX = EnvDataInterp(:,8);
+    PayloadRadData{i}.GyroY = EnvDataInterp(:,9);
+    PayloadRadData{i}.GyroZ = EnvDataInterp(:,10);
+    PayloadRadData{i}.MagX = EnvDataInterp(:,11);
+    PayloadRadData{i}.MagY = EnvDataInterp(:,12);
+    PayloadRadData{i}.MagZ = EnvDataInterp(:,13);
+    PayloadRadData{i}.IMUTemp = EnvDataInterp(:,14);
+    PayloadRadData{i}.HPSTemp = EnvDataInterp(:,15);
+    PayloadRadData{i}.EXTTemp = EnvDataInterp(:,16);
+    PayloadRadData{i}.BATTemp = EnvDataInterp(:,17);
+    PayloadRadData{i}.PMTTemp = EnvDataInterp(:,18);
+    PayloadRadData{i}.gpsPacketNums = EnvDataInterp(:,19);
+    PayloadRadData{i}.gpsTimes = EnvDataInterp(:,20);
+    PayloadRadData{i}.gpsLats = EnvDataInterp(:,21);
+    PayloadRadData{i}.gpsLongs = EnvDataInterp(:,22);
+    PayloadRadData{i}.gpsSpeeds = EnvDataInterp(:,23);
+    PayloadRadData{i}.gpsAngles = EnvDataInterp(:,24);
+    PayloadRadData{i}.gpsAlts = EnvDataInterp(:,25);
+    PayloadRadData{i}.gpsSatNum = EnvDataInterp(:,26);
+    PayloadRadData{i}.gpsLatErrs = EnvDataInterp(:,27);
+    PayloadRadData{i}.gpsLongErrs = EnvDataInterp(:,28);
+    PayloadRadData{i}.gpsAltErrs = EnvDataInterp(:,29);
+    PayloadRadData{i}.gpsClkBiases = EnvDataInterp(:,30);
+    PayloadRadData{i}.gpsClkDrifts = EnvDataInterp(:,31);
+    PayloadRadData{i}.TimeErrs = EnvDataInterp(:,32);
 end
 
 
