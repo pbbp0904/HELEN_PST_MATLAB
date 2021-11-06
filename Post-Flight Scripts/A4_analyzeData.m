e60b39e2c581ba9b976af4153997753f567fa889
pbbp0904 <helmerichchristopher0904@gmail.com>
2021-11-05 20:24:23 -0400

    added Analyze Data script

---
+++ Post-Flight Scripts/A4_analyzeData.m
@@ -0,0 +1,12 @@
+%% Go through the radiation data and look for cross corelations of events
+% between payloads
+
+% Distance between payloads throughout the flight
+[xEast,yNorth,zUp] = latlon2local(lat,lon,alt,origin);
+
+% Get number of counts for each time bin throughout the flight
+
+
+
+
+
---
+++ Post-Flight Scripts/calcDistance.m
@@ -0,0 +1,6 @@
+function [distance] = calcGPSDistance(lat1,long1,alt1,lat2,long2,alt2)
+
+
+
+
+end
