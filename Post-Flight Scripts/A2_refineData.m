017440908dc2a7092ba26a2a0deaf4341ada38f2
xcoza <xcoza@DESKTOP-HM88LRH>
2021-11-10 16:02:23 -0600

    directory change

--- Post-Flight Scripts/A2_refineData.m
+++ Post-Flight Scripts/A2_refineData.m
@@ -4,7 +4,7 @@
 % Fix sub second calculation
 
 %%
-DirectoryLocation = "D:/Flight Data/Flight 3/3-Processed Data/";
+DirectoryLocation = "D:\MATLAB\HELEN Data\Flight 2\3-Processed Data\";
 tic
 
 if ~exist('PayloadEnvData','var')
