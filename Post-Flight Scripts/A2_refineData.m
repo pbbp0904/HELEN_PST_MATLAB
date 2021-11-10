f404cdd9da92058c3726bd907f3c9ba7dd074061
pbbp0904 <helmerichchristopher0904@gmail.com>
2021-11-10 17:52:48 -0500

    Added backslash

--- Post-Flight Scripts/A2_refineData.m
+++ Post-Flight Scripts/A2_refineData.m
@@ -7,7 +7,7 @@
 FlightFolder = "D:\Flight Data\Flight 2\";
 %FlightFolder = "D:\MATLAB\HELEN Data\Flight 2\";
 
-DirectoryLocation = strcat(FlightFolder,"3-Processed Data");
+DirectoryLocation = strcat(FlightFolder,"3-Processed Data\");
 tic
 
 if ~exist('PayloadEnvData','var')
