6917e8abfa2d44d746fe4d632b93173427a5f624
pbbp0904 <helmerichchristopher0904@gmail.com>
2021-11-19 18:19:47 -0500

    Removed filepaths

--- Post-Flight Scripts/A1_parseData.m
+++ Post-Flight Scripts/A1_parseData.m
@@ -7,10 +7,6 @@
 %%
 clear; clc; close all;
 
-%FlightFolder = "D:\Flight Data\Testing\Database Test\";
-%FlightFolder = "C:\Users\Sean\Desktop\UAH\SHC\HELEN\HELEN_Data\Flight 2\";
-%FlightFolder = "D:\Flight Data\Flight 2\";
-%FlightFolder = "D:\MATLAB\HELEN Data\Flight 2\";
 FlightFolder = runFile();
 
 if isfolder(FlightFolder + '3-Processed Data') == 0
