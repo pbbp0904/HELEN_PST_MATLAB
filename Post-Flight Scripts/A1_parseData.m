1da5b8d7404395d2a3eb573b88ab825f99017200
Adelwolf92 <seanhassler1@protonmail.com>
2021-11-19 17:46:23 -0600

    Added comments to FileFolder.

--- Post-Flight Scripts/A1_parseData.m
+++ Post-Flight Scripts/A1_parseData.m
@@ -1,9 +1,6 @@
 %% A1 Todo
 
 
-% Make a run file - Sean - Create a text file with the data directory in it, have
-% MATLAB load file, and set FlightFolder variable to the data directory 
-
 %%
 clear; clc; close all;
 
