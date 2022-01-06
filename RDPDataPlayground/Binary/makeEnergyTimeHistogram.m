d77bc0b2ac8d30c0c83d9a7cd7068616dd8d54a1
pbbp0904 <helmerichchristopher0904@gmail.com>
2022-01-06 16:02:43 -0500

    Added some 2D histogram analysis

---
+++ RDPDataPlayground/Binary/makeEnergyTimeHistogram.m
@@ -0,0 +1,24 @@
+payloadNumber = 2;
+b = PayloadRadData{payloadNumber}.pulsedata_b;
+b = b(boolean(~PayloadRadData{payloadNumber}.isTail),:);
+b = b(max(abs(b'))<3000,:);
+b = b(1:end,:);
+
+
+d = PayloadRadData{payloadNumber}.pps_count;
+d = d(boolean(~PayloadRadData{payloadNumber}.isTail),:);
+d = d(max(abs(b'))<3000,:);
+d = d(1:end,:);
+b = b(d~=0,:);
+d = d(d~=0,:);
+
+
+m = max(-b');
+h=hist3([m',d],[round((max(m)-min(m))/5),round((max(d)-min(d))/60)],'EdgeColor','none','CDataMode','auto','FaceColor','interp');
+imagesc(0:max(d),min(m):max(m),h,'AlphaData',h)
+colormap('jet')
+set(gca, 'ColorScale', 'log')
+set(gca,'YDir','normal')
+caxis([1, max(max(h))])
+colorbar
+view(2)
